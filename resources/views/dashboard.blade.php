02c1a5af47b937cd98078d0bc2fb64b52abccd71
El maic <adrian@develupme.com>
2019-10-22 04:21:34 -0500

    dashboard con sesiones y plantilla

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.interior')
+
+
+@section('content')
+
+@include('layouts.menu')
+
+@include('layouts.sidebar')
+
+
+<div class="main-panel">
+    <div class="content-wrapper">
+            <!-- Page Title Header Starts-->
+            <div class="row page-title-header">
+              <div class="col-12">
+                <div class="page-header">
+                  <h4 class="page-title">Dashboard</h4>
+                  <div class="quick-link-wrapper w-100 d-md-flex flex-md-wrap">
+                    <ul class="quick-links">
+                      <li><a href="#">ICE Market data</a></li>
+                      <li><a href="#">Own analysis</a></li>
+                      <li><a href="#">Historic market data</a></li>
+                    </ul>
+                    <ul class="quick-links ml-auto">
+                      <li><a href="#">Settings</a></li>
+                      <li><a href="#">Analytics</a></li>
+                      <li><a href="#">Watchlist</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+    </div>
+</div>            
+
+
+
+@endsection
